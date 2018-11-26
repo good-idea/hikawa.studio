e1e31e75b71ddb4d3fcc4a784394f8c2e644e143
Joseph Thomas <joseph@good-idea.studio>
2018-11-25 20:12:34 -0800

    chore(proxy): update dependencies & ignore dist

--- proxy/dist/server.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-require('dotenv').config({
-	path: path.resolve(__dirname, '.env'),
-})
-
-console.log(process.env.NODE_ENV)
-const runServer = require('@good-idea/shopify-sanity-proxy-server').default
-
-runServer()
