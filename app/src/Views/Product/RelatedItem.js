28feca2636de47f2aca063b969bdef99501aba62
Joseph Thomas <joseph@good-idea.studio>
2018-12-10 16:41:53 -0800

    fix(app): fix related item image sizing

--- app/src/Views/Product/RelatedItem.js
+++ app/src/Views/Product/RelatedItem.js
@@ -7,6 +7,7 @@ import PageLink from 'Components/ContentBlocks/PageLink'
 const Wrapper = styled.div`
 	${({ theme }) => css`
 		margin: 0 ${theme.layout.spacing.single};
+		flex-basis: 33%;
 	`}
 `
 
